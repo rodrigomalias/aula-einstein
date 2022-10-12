5deaccaced6ceb96974b3f04d3ea2f1133f62ce7
Rodrigo <36937071+rodrigomalias@users.noreply.github.com>
2022-10-11 22:25:07 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,6 @@
+const EnviarFormulario = () => {
+  nome = document.getElementById("nome").value
+  email = document.getElementById("email").value
+  mensagem = document.getElementById("mensagem").value
+  console.log({nome, email, mensagem})
+}
